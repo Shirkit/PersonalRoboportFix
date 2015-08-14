257301d77744a818ea349c394786b46f40d06458
Shirkit <shirkit@gmail.com>
2015-08-14 14:19:03 -0300

    Added support for multiple personal roboports Fixed crash when removing armor

--- control.lua
+++ control.lua
@@ -1,3 +1,9 @@
+-- Configuration
+
+TICKS = 20 -- Every X ticks the mod will run an update. The higher X is, the less CPU it consumes, but the slower the mod gets. Default: 20
+
+-- Do not edit from here below
+
 require "defines"
 require "util"
 
@@ -8,7 +14,7 @@ function initContent()
 	
 	for i,player in ipairs(game.players) do
 		if global.roboports[player.name] == nil then
-			global.roboports[player.name] = false
+			global.roboports[player.name] = { modules = { } }
 		end
 	end
 end
@@ -28,38 +34,54 @@ game.on_event(defines.events.on_player_created, function(event)
 	initContent()
 end)
 
+--[[
+
+global.roboports[string]
+- modules[] = name of the module
+
+]]--
+
 
 game.on_event(defines.events.on_tick, function(event)
 
-	if event.tick %15 == 7 then
+	if event.tick %TICKS == 0 then
 	
-		local surface = game.get_surface(1)
 		for i,player in ipairs(game.players) do
 			
-			local roboport = getPersonalRoboport(player)
-			if roboport ~= nil then
-				local grid = getArmorGrid(player)
+			local result = getPersonalRoboport(player)
+			if result ~= nil then
 				
-				if roboport.name == "personal-roboport-equipment-off" then
-					-- roboport is turned off
-					if not isInRoboportRange(surface, player.position) then
-						local prevEnergy = roboport.energy
-						grid.take(roboport)
-						local put = grid.put({name = global.roboports[player.name]})
-						put.energy = prevEnergy
-						-- turn it on
-					end
-				else
-					-- we have a roboport turned on
-					if (countRobots(player) >= 10) then
-						if (isInRoboportRange(surface, player.position)) then
-							local prevEnergy = roboport.energy
-							global.roboports[player.name] = grid.take(roboport).name
-							local put = grid.put({name = "personal-roboport-equipment-off"})
+				local surface = game.get_surface(1)
+				local roboport = result.equips
+				local grid = getArmorGrid(player)
+				local i = 1
+				
+				while roboport[i] ~= nil do
+					if roboport[i].name == "personal-roboport-equipment-off" then
+						-- roboport is turned off
+						if not isInRoboportRange(surface, player.position) then
+							local prevEnergy = roboport[i].energy
+							local pos = roboport[i].position
+							-- Swap items and properties
+							grid.take( {position = pos} )
+							local put = grid.put({name = global.roboports[player.name].modules[i], position = pos})
 							put.energy = prevEnergy
-							-- turn it off
+						end
+					else
+						-- roboport is turned on
+						if (countRobots(player) >= result.bots) then
+							if (isInRoboportRange(surface, player.position)) then
+								local prevEnergy = roboport[i].energy
+								local pos = roboport[i].position
+								-- Swap items and properties
+								global.roboports[player.name].modules[i] = grid.take( {position = pos} ).name
+								local put = grid.put( {name = "personal-roboport-equipment-off", position = pos} )
+								put.energy = prevEnergy
+							end
 						end
 					end
+					
+					i = i + 1
 				end
 			end
 		end
@@ -67,6 +89,7 @@ game.on_event(defines.events.on_tick, function(event)
 	
 end)
 
+-- Returns the number of robots in the player's main inventory and quickbar
 function countRobots(player)
 
 	local quick = player.get_inventory(defines.inventory.player_quickbar)
@@ -93,28 +116,48 @@ function countRobots(player)
 end
 
 
--- Returns a Lua/Equipment or nil if not found
+--[[
+ Returns a struct or nil if not found
+{
+	count = number of roboports
+	equips[] = array of Lua/Equipment
+	bots = maximum number of bots
+ }
+ ]]--
 function getPersonalRoboport(player)
 	local grid = getArmorGrid(player)
+	local i = 1
+	local bot = 0
+	local arr = { }
+	
 	if grid ~= nil then
 		local equips = grid.equipment
 		for v,k in ipairs(equips) do
 			if (k.type == "roboport-equipment") then
-				return k
+				arr[i] = k
+				bot = bot + 10
+				i = i + 1
 			end
 		end
 	end
-	return nil
+	
+	if i == 1 then
+		return nil
+	else
+		return { count = i - 1, bots = bot, equips = arr }
+	end
 end
 
+-- Returns a Lua/EquipmentGrid of the current's player armor
 function getArmorGrid(player)
 	local armor = player.get_inventory(defines.inventory.player_armor)
-	if armor ~= nil and armor[1].has_grid then
+	if armor ~= nil and armor[1].valid_for_read and armor[1].has_grid then
 		return armor[1].grid
 	end
 	return nil
 end
 
+-- Checks if player is inside a construction network
 function isInRoboportRange(surface,position)
 	--Find Roboports
 	local entities = surface.find_entities_filtered{area = {{position.x - 51, position.y - 51}, {position.x + 51, position.y + 51}}, type = "roboport"}
@@ -126,6 +169,7 @@ function isInRoboportRange(surface,position)
 	return false
 end
 
+-- Just here for debug
 function debugLog(message)
 	for i,player in ipairs(game.players) do
 		if true then -- set for debug
